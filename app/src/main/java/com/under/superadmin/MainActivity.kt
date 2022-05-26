de9a89e80e9b7b6f844940743a907b423a3c9800
JhonS <jhonsaldarriagalopez@gmail.com>
2022-05-26 02:25:10 -0500

    recoveryPassword has been added in LoginActivity

--- app/src/main/java/com/under/superadmin/MainActivity.kt
+++ app/src/main/java/com/under/superadmin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.under.superadmin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.under.superadmin.databinding.ActivityMainBinding
